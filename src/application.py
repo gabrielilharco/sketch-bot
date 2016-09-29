34a796b28d7a14b6518475204a7f19a9ca985c9b
gabrielilharco <gabriel.ilharco@gmail.com>
2016-09-29 10:12:14 -0300

    Add random sample image display

--- src/application.py
+++ src/application.py
@@ -1,4 +1,5 @@
 from data_processing import *
 
 if __name__ == '__main__':
-	maybe_extract('data.tar.gz', 250)
+	folders = maybe_extract('data.tar.gz', 250)
+	display_random_image('data')
--- src/data_processing.py
+++ src/data_processing.py
@@ -1,12 +1,17 @@
 import os
 import sys
 import tarfile
+import numpy as np
+import Image
 
 def maybe_extract(filename, num_classes, force=False):
-	# getting path root
+	"""
+	extract data from a file if it isn't already extracted.
+	extraction can be forced with the 'force parameter'.
+	returns a list of folders in the extracted file;
+	"""
 	root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .zip
 	if os.path.isdir(root) and not force:
-		# You may override by setting force=True.
 		print('%s already present - Skipping extraction...' % (root))
 	else:
 		print('Extracting data. This may take a while...')
@@ -22,3 +27,17 @@ def maybe_extract(filename, num_classes, force=False):
 			'Expected %d folders, one per class. Found %d instead.' % (
 				num_classes, len(data_folders)))
 	return data_folders
+
+def display_random_image(root, class_name=None):
+	"""
+	display a random image in a root directory.
+	class_name is the name of the subdirectory
+	if class_name is set to None, then a random folder is chosen
+	"""
+	if class_name is None:
+		class_name = np.random.choice(os.listdir(root))
+	print('Showing image of class <%s>' % class_name)
+	folder = root + '/' + class_name
+	image_file = np.random.choice(os.listdir(folder))
+	image = Image.open(folder+'/'+image_file)
+	image.show()
