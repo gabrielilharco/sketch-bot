4705c0b5c7b4f335e01c0c1950e3008759c8267a
gabrielilharco <gabriel.ilharco@gmail.com>
2016-10-01 19:45:13 -0300

    Load and pickle data

--- src/application.py
+++ src/application.py
@@ -2,5 +2,4 @@ from data_processing import *
 
 if __name__ == '__main__':
 	folders = maybe_extract('data.tar.gz', 250)
-	#display_random_image('data')
-	scale_and_trim('data/airplane/1.png', 400, 400, 20)
+	dataset = maybe_pickle(folders, 128, 128, 2)
--- src/data_processing.py
+++ src/data_processing.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import tarfile
+import pickle
 import numpy as np
 from PIL import Image, ImageOps
 from scipy import ndimage
+
 
 def maybe_extract(filename, num_classes, force=False):
 	"""
@@ -12,14 +14,13 @@ def maybe_extract(filename, num_classes, force=False):
 	returns a list of folders in the extracted file;
 	"""
 	root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .zip
-	if os.path.isdir(root) and not force:
-		print('%s already present - Skipping extraction...' % (root))
-	else:
-		print('Extracting data. This may take a while...')
+	if force or not os.path.isdir(root):
+		# extract data
 		tar = tarfile.open(filename)
 		sys.stdout.flush()
 		tar.extractall()
 		tar.close()
+	# list of folders
 	data_folders = [
 		os.path.join(root, d) for d in sorted(os.listdir(root))
 		if os.path.isdir(os.path.join(root, d))]
@@ -69,13 +70,35 @@ def scale_and_trim(image_file, width, height, padding=0):
 	# returning
 	return np.asarray(scaled_image)
 
-def load_images(folder, image_size,  min_num_images):
+def load_images(folder, width, height, padding = 0):
 	"""
 	load data for a single class (inside a folder)
 	"""
-	raise NotImplementedError
-	# image_files = os.listdir(folder)
-	# dataset = np.ndarray(shape=(len(image_files), image_size, image_size), dtype=np.float32)
-	# num_images = 0
-	# for image in image_files:
-	# 	image_file = os.path.join(folder, image)
+	image_files = os.listdir(folder)
+	dataset = np.ndarray(shape=(len(image_files), width, height), dtype=np.float32)
+	num_images = 0
+	for i in range(len(image_files)):
+		image_file = os.path.join(folder, image_files[i])
+		image_data = scale_and_trim(image_file, width, height, padding)
+		dataset[i, :, :] = image_data
+	return dataset
+
+def maybe_pickle(data_folders, width, height, padding = 0, force = False):
+	"""
+	load all data given a list of folders each representing a class
+	pickle the data if it is not yet pickled or if force is enabled
+	"""
+	print ('Pickling data. This may take a while...')
+	dataset_names = []
+	for folder in data_folders:
+		pickled_folder = folder + '.pickle'
+		dataset_names.append(pickled_folder)
+		if force or not os.path.exists(pickled_folder):
+			# pickle!
+			dataset = load_images(folder, width, height, padding)
+			try:
+				with open (pickled_folder, 'wb') as f:
+					pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+			except Exception as e:
+				print ('Error while pickling data to', pickled_folder, ':', e)
+	return dataset_names
