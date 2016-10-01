483e23ab3330e49647f211773b5a780ac317893f
gabrielilharco <gabriel.ilharco@gmail.com>
2016-10-01 15:03:40 -0300

    Add scale and trim for images

--- src/application.py
+++ src/application.py
@@ -2,4 +2,5 @@ from data_processing import *
 
 if __name__ == '__main__':
 	folders = maybe_extract('data.tar.gz', 250)
-	display_random_image('data')
+	#display_random_image('data')
+	scale_and_trim('data/airplane/1.png', 400, 400, 20)
--- src/data_processing.py
+++ src/data_processing.py
@@ -2,7 +2,8 @@ import os
 import sys
 import tarfile
 import numpy as np
-import Image
+from PIL import Image, ImageOps
+from scipy import ndimage
 
 def maybe_extract(filename, num_classes, force=False):
 	"""
@@ -37,7 +38,44 @@ def display_random_image(root, class_name=None):
 	if class_name is None:
 		class_name = np.random.choice(os.listdir(root))
 	print('Showing image of class <%s>' % class_name)
-	folder = root + '/' + class_name
+	folder = os.path.join(root, class_name)
 	image_file = np.random.choice(os.listdir(folder))
-	image = Image.open(folder+'/'+image_file)
+	image = Image.open(os.path.join(folder,image_file))
 	image.show()
+
+def scale_and_trim(image_file, width, height, padding=0):
+	"""
+	scale image to a given width and height.
+	the first step is to find a boundbox containing the actual drawing in the image
+	after that, just a simple scaling, considering padding if it is the case
+	"""
+	# actual width and height we need to re-scale to
+	w = width-2*padding
+	h = height-2*padding
+	# read image
+	image = Image.open(image_file)
+	# trimming
+	xstart, ystart, xend, yend = ImageOps.invert(image).getbbox()
+	trimmed_image = image.crop((xstart, ystart, xend, yend))
+	# scale 
+	scale_factor = min(float(w)/(yend-ystart), float(h)/(xend-xstart))
+	size = (int(scale_factor*(xend-xstart)), int(scale_factor*(yend-ystart)))
+	trimmed_image.thumbnail(size, Image.ANTIALIAS)
+	# expanding
+	t_w, t_h = trimmed_image.size
+	scaled_image = Image.new('L', (width, height), color=255)
+	offset = ((width-t_w)/2, (height-t_h)/2)
+	scaled_image.paste(trimmed_image, offset)
+	# returning
+	return np.asarray(scaled_image)
+
+def load_images(folder, image_size,  min_num_images):
+	"""
+	load data for a single class (inside a folder)
+	"""
+	raise NotImplementedError
+	# image_files = os.listdir(folder)
+	# dataset = np.ndarray(shape=(len(image_files), image_size, image_size), dtype=np.float32)
+	# num_images = 0
+	# for image in image_files:
+	# 	image_file = os.path.join(folder, image)
