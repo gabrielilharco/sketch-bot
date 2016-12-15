0b4fcb313a633260b3f4cb795d4f627fcd0f31cc
gabrielilharco <gabriel.ilharco@gmail.com>
2016-12-15 17:27:53 -0200

    CNN ~45% acc

--- src/application.py
+++ src/application.py
@@ -3,4 +3,6 @@ from visual_vocabulary import *
 
 if __name__ == '__main__':
 	folders = maybe_extract('data.tar.gz', 250)
-	X, y = load_images(['trim_'+ folder for folder in folders])
+	size = 48
+	trim_images(folders, size, size, 2)
+	X, y = load_images(['trim_'+ folder for folder in folders], size, size)
